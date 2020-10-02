0b240f0c1ec7ecf4a9fd26a96f3e4e73455027ca
Jonathan Taylor <taylorjd2@cardiff.ac.uk>
2020-10-02 22:15:23 +0100

    slow solution

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,82 @@
 const { once } = require('events');
 const { createReadStream } = require('fs');
 const { createInterface } = require('readline');
+const fs = require('fs').promises;
 
-
-//count occurences
-// [{name: count}, {name:count}]
 class NameCounter {
     private _name: string;
     private _count: number = 0;
-
     constructor(name: string) {this._name = name;}
-
     get name(): string {return this._name;}
     get count(): number {return this._count;}
-
     incrementCounter() {this._count++}
   }
 
-
   let counterArray: Array<NameCounter> = []
-  let counter: number = 0;
+  // let counter: number = 0; //test
 
-  const fileReader =()=>{  
-    let lineReaderText = require('readline').createInterface({
-      input: require('fs').createReadStream('./oliver-twist.txt')
-    });
-    lineReaderText.on('line', function (line: string) {
-      counterArray.forEach((nameCounter, i)=>{
-        let name = nameCounter.name;
-        // let name = 'Andrew'
-        let uppercaseName = name.toUpperCase();
-        let testVariable = '('+name+'|'+uppercaseName+')'
+  const countNames = async ()=>{  
+    try {
+      let rl = createInterface({
+        input: createReadStream('./oliver-twist.txt')
+      });
+      rl.on('line', (line: string) => {
+        counterArray.forEach((nameCounter, i)=>{
+          let name = nameCounter.name;
+          // let name = 'Andrew' //test
+          let uppercaseName = name.toUpperCase();
+          let testVariable = '('+name+'|'+uppercaseName+')'
 
-        // let re = new RegExp(testVariable);
-        let re2 = new RegExp('(.*[\W_\'(",-]+|^)'+testVariable+'([.,_";!:\?-]+.*$|[\?,]\'$|[\!\?<>\":;{}\[\]@£$%^&\*()_+=,-]+\'|\'[sS][\!\?<>\":;{}\[\]@£$%^&\*()_+=,-]+.*|\'[Ss]|\'$|$)');
-       
-        //Oliver is working now
-        //Test Don
-        //Test all others
-        //Simplify regex
-        //Make faster
-    
-        
-        
-        
-        
-        let lineArray: Array<string> = line.split(' ').filter(word=> re2.test(word) ? word:null);
-        // let lineArray: Array<string> = line.split(' ').filter(word=> re.test(word) && !re2.test(word) ? word:null);
-        lineArray.forEach(word=>{
-          // console.log(word);
-          // counter++
-          counterArray[i].incrementCounter()
+          // let re = new RegExp(testVariable);//test
+          let re2 = new RegExp('(.*[\W_\'(",-]+|^)'+testVariable+'([.,_";!:\?-]+.*$|[\?,]\'$|[\!\?<>\":;{}\[\]@£$%^&\*()_+=,-]+\'|\'[sS][\!\?<>\":;{}\[\]@£$%^&\*()_+=,-]+.*|\'[Ss]|\'$|$)');
+          
+          let lineArray: Array<string> = line.split(' ').filter(word=> re2.test(word) ? word:null);
+          // let lineArray: Array<string> = line.split(' ').filter(word=> re.test(word) && !re2.test(word) ? word:null); //test
+          lineArray.forEach(word=>{
+            // console.log(word);//test
+            // counter++//test
+            counterArray[i].incrementCounter()
         }) 
       })  
     });
-    lineReaderText.on('close', () => {
-      console.log('finished');
-      // console.log(counter);
-      let sortedArray = counterArray.sort((nc1, nc2)=>{
-        const count1 = nc1.count;
-        const count2 = nc2.count;
-        if(count1 > count2){return 1;}
-        if(count1 < count2){return -1;}
-        return 0;
-      })
-      sortedArray.forEach((nameCounter) => {
-        let name = nameCounter.name;
-        let countTotal = nameCounter.count;
-          console.log(name+": "+ countTotal);
-      });
+    await once(rl, 'close');
+    console.log('finished reading');
+      // console.log(counter);//test
+      let sortedArray = await sortArray(counterArray)
+      let text = "";
+      for (let i = 0; i < sortedArray.length; i++) {
+        text += sortedArray[i].name + ": " + sortedArray[i].count + "\n"
+      }
+
+      writeToFile('./counted-names.txt', text)
+      //[{name: counter}, {name: counter}]
+      //"name: counter/n name:counter/n"
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const sortArray = async (nameCounterArray:Array<NameCounter>)=>{
+    let sortedArray = nameCounterArray.sort((nc1, nc2)=>{
+      const count1 = nc1.count;
+      const count2 = nc2.count;
+      if(count1 > count2){return 1;}
+      if(count1 < count2){return -1;}
+      return 0;
     })
-  }  
+    return sortedArray;
+  }
+
+  const writeToFile = async (filePath: string, text: string)=>{
+    try {
+      await fs.writeFile(filePath, text); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
-  (async function processLineByLine() {
+  (async function fileToArray() {
     try {
       const rl = createInterface({
         input: createReadStream('first-names.txt'),
@@ -86,15 +89,12 @@ class NameCounter {
   
       await once(rl, 'close');
 
-      fileReader();
+      countNames();
 
     } catch (err) {
       console.error(err);
     }
   })();
-
-  
-  // sort the array based on the count property
 
 
   //steps
@@ -112,14 +112,5 @@ class NameCounter {
   //2. not to remove each array that has length of 0 (saves having to find the length of each array)
   //3. realised that sometimes the first letter may not be a capital
   //4. regex cannot ignore case of name in text. Because names like Soon
-
-
-//1. Todo
-//1. make regex as specific as possible
-//2. test with other names and make sure there are no additional parts of regex that should be added
-
-//18000 words
-//100 names
-
-//either check each word 100 times
-//check each name 18000 times
+  //5. changed regex so that Don't was not returned for Don but Don's would still be returned
+  //6. Make faster
