c24bf546cb9989b13857f9da71600bdc4ed4191d
Jonathan Taylor <taylorjd2@cardiff.ac.uk>
2020-10-03 21:12:26 +0100

    code clean up

--- src/index.ts
+++ src/index.ts
@@ -9,53 +9,61 @@ class NameCounter {
     constructor(name: string) {this._name = name;}
     get name(): string {return this._name;}
     get count(): number {return this._count;}
-    incrementCounter() {this._count++}
+    incrementCounter(amount:number) {this._count+=amount}
   }
 
-  let counterArray: Array<NameCounter> = []
-  // let counter: number = 0; //test
+  let counterArray: Array<NameCounter> =[];
 
-  const countNames = async ()=>{  
-    try {
-      let rl = createInterface({
-        input: createReadStream('./oliver-twist.txt')
-      });
-      rl.on('line', (line: string) => {
-        counterArray.forEach((nameCounter, i)=>{
-          let name = nameCounter.name;
-          // let name = 'Andrew' //test
-          let uppercaseName = name.toUpperCase();
-          let testVariable = '('+name+'|'+uppercaseName+')'
+  //converts the first names file into an array of NameCounter instances
+  const fileToArray = async (fileName: string)=> {
+    return new Promise(async (resolve, reject)=>{
+      try {
+        // let counter: number = 0; //test
+        const rl = createInterface({input: createReadStream(fileName)});
+        rl.on('line', (line: string) => counterArray.push(new NameCounter(line)))
 
-          // let re = new RegExp(testVariable);//test
-          let re2 = new RegExp('(.*[\W_\'(",-]+|^)'+testVariable+'([.,_";!:\?-]+.*$|[\?,]\'$|[\!\?<>\":;{}\[\]@£$%^&\*()_+=,-]+\'|\'[sS][\!\?<>\":;{}\[\]@£$%^&\*()_+=,-]+.*|\'[Ss]|\'$|$)');
-          
-          let lineArray: Array<string> = line.split(' ').filter(word=> re2.test(word) ? word:null);
-          // let lineArray: Array<string> = line.split(' ').filter(word=> re.test(word) && !re2.test(word) ? word:null); //test
-          lineArray.forEach(word=>{
-            // console.log(word);//test
-            // counter++//test
-            counterArray[i].incrementCounter()
-        }) 
-      })  
-    });
-    await once(rl, 'close');
-    console.log('finished reading');
-      // console.log(counter);//test
-      let sortedArray = await sortArray(counterArray)
-      let text = "";
-      for (let i = 0; i < sortedArray.length; i++) {
-        text += sortedArray[i].name + ": " + sortedArray[i].count + "\n"
+        await once(rl, 'close');
+        resolve();
+      } catch (err) {
+        console.log(err);
+        reject()
       }
+    })};
 
-      writeToFile('./counted-names.txt', text)
-      //[{name: counter}, {name: counter}]
-      //"name: counter/n name:counter/n"
-    } catch (err) {
-      console.error(err);
-    }
+
+
+  //counts the number of times each name appears in the txt file
+  const countNames = async (fileName: string)=>{
+    return new Promise(async (resolve, reject)=>{
+      try {
+        let rl = createInterface({input: createReadStream(fileName)});
+        rl.on('line', (line: string) => {
+          for (let i = 0; i < counterArray.length; i++) {          
+            let name = counterArray[i].name;
+            // let name = 'Harry' //test
+            let uppercaseName = name.toUpperCase();
+            let testName = '('+name+'|'+uppercaseName+')'
+            // let re = new RegExp(testName);//test
+            let re2 = new RegExp('(^|["\'_({\[-]+)'+testName+'([?,"_\)\}.;!:\\]-]|\'[sS]|$)');
+            
+            let lineArray: Array<string> = line.split(' ').filter(word=> re2.test(word) ? word:null);
+            // let lineArray: Array<string> = line.split(' ').filter(word=> re.test(word) && !re2.test(word) ? word:null); //test
+            counterArray[i].incrementCounter(lineArray.length)
+  
+              // console.log(word);//test
+              // counter++//test
+          }
+      });
+      await once(rl, 'close');
+        // console.log(counter);//test
+        resolve();
+      } 
+      catch (err) {console.log(err);}
+    })
   }
+   
 
+  //sorts array ascending order
   const sortArray = async (nameCounterArray:Array<NameCounter>)=>{
     let sortedArray = nameCounterArray.sort((nc1, nc2)=>{
       const count1 = nc1.count;
@@ -67,31 +75,41 @@ class NameCounter {
     return sortedArray;
   }
 
-  const writeToFile = async (filePath: string, text: string)=>{
-    try {
-      await fs.writeFile(filePath, text); 
-    } catch (error) {
-      console.log(error)
+  //converts the NameCounter array to a string of name: count \n name: count /n...
+  const arrayToString = (sortedArray: Array<NameCounter>)=>{
+    return new Promise<string>((resolve, reject)=>{
+      try{
+        let text = "";
+        for (let i = 0; i < sortedArray.length; i++) {
+          text += sortedArray[i].name + ": " + sortedArray[i].count + "\n"
+        }
+        resolve(text)
     }
+    catch(err){
+      console.log(err)
+      reject()
+    }
+    })
   }
-
-
-  (async function fileToArray() {
-    try {
-      const rl = createInterface({
-        input: createReadStream('first-names.txt'),
-        crlfDelay: Infinity
-      });
-      rl.on('line', (line: string) => {
-          let newCounter = new NameCounter(line);
-          counterArray.push(newCounter);
-        })
-  
-      await once(rl, 'close');
-
-      countNames();
-
-    } catch (err) {
-      console.error(err);
+    //writes the string to a txt file
+    const writeToFile = async (filePath: string, text: string)=>{
+      try {
+        await fs.writeFile(filePath, text);
+      } catch (error) {console.log(error)}
     }
-  })();
+
+  (async function () {
+    try{
+      console.log("reading names...");
+      await fileToArray('first-names.txt')
+      console.log("reading Oliver Twist...");
+      await countNames('./oliver-twist.txt');
+      console.log("manipulating data...");
+      let sortedArray = await sortArray(counterArray)
+      let text = await arrayToString(sortedArray);
+      console.log("writing to txt file...");
+      writeToFile('./counted-names.txt', text)
+      console.log("complete");
+    }
+    catch (error){console.log(error);}
+  })()
